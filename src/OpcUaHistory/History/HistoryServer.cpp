e9946d1a985326a52efe4818bcb5047116533b38
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 10:56:59 +0200

    added new class HistoryServer

---
+++ src/OpcUaHistory/History/HistoryServer.cpp
@@ -0,0 +1,24 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaHistory/History/HistoryServer.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaHistory
+{
+
+	HistoryServer::HistoryServer(void)
+	{
+	}
+
+	HistoryServer::~HistoryServer(void)
+	{
+	}
+
+}
---
+++ src/OpcUaHistory/History/HistoryServer.h
@@ -0,0 +1,25 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaHistory_HistoryServer_h__
+#define __OpcUaHistory_HistoryServer_h__
+
+namespace OpcUaHistory
+{
+
+	class HistoryServer
+	{
+	  public:
+		HistoryServer(void);
+		~HistoryServer(void);
+
+	  private:
+	};
+
+}
+
+#endif
