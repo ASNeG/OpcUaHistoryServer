68808d3238f31811c9921731d2c562543f80b9a7
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 10:54:10 +0200

    added new class HistoryClient

---
+++ src/OpcUaHistory/History/HistoryClient.cpp
@@ -0,0 +1,24 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaHistory/History/HistoryClient.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaHistory
+{
+
+	HistoryClient::HistoryClient(void)
+	{
+	}
+
+	HistoryClient::~HistoryClient(void)
+	{
+	}
+
+}
---
+++ src/OpcUaHistory/History/HistoryClient.h
@@ -0,0 +1,25 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaHistory_HistoryClient_h__
+#define __OpcUaHistory_HistoryClient_h__
+
+namespace OpcUaHistory
+{
+
+	class HistoryClient
+	{
+	  public:
+		HistoryClient(void);
+		~HistoryClient(void);
+
+	  private:
+	};
+
+}
+
+#endif
