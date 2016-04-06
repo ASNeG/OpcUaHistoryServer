49c4a16c2634c482b9910da5b4749925096fe026
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 10:47:37 +0200

    added new class HistoryConfig

---
+++ src/OpcUaHistory/History/HistoryConfig.cpp
@@ -0,0 +1,24 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaHistory/History/HistoryConfig.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaHistory
+{
+
+	HistoryConfig::HistoryConfig(void)
+	{
+	}
+
+	HistoryConfig::~HistoryConfig(void)
+	{
+	}
+
+}
---
+++ src/OpcUaHistory/History/HistoryConfig.h
@@ -0,0 +1,25 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaHistory_HistoryConfig_h__
+#define __OpcUaHistory_HistoryConfig_h__
+
+namespace OpcUaHistory
+{
+
+	class HistoryConfig
+	{
+	  public:
+		HistoryConfig(void);
+		~HistoryConfig(void);
+
+	  private:
+	};
+
+}
+
+#endif
