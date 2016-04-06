ca9afc1d875fbe438c8070dbc86a57f15d044cef
Kai Huebl <kai@huebl-sgh.de>
2016-04-06 12:26:49 +0200

    added new class FileManager

---
+++ src/OpcUaHistory/HistoryAccessFile/FileManager.cpp
@@ -0,0 +1,24 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaHistory/HistoryAccessFile/FileManager.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaHistory
+{
+
+	FileManager::FileManager(void)
+	{
+	}
+
+	FileManager::~FileManager(void)
+	{
+	}
+
+}
---
+++ src/OpcUaHistory/HistoryAccessFile/FileManager.h
@@ -0,0 +1,29 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+   Alle Rechte vorbehalten.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaHistory_FileManager_h__
+#define __OpcUaHistory_FileManager_h__
+
+#include "OpcUaHistory/HistoryAccess/HistoryAccessIf.h"
+
+using namespace OpcUaStackCore;
+
+namespace OpcUaHistory
+{
+
+	class FileManager
+	{
+	  public:
+		FileManager(void);
+		~FileManager(void);
+
+	  private:
+	};
+
+}
+
+#endif
