3257572709beacd6c663d31dca8b036e48ddd120
Kai Huebl <kai@huebl-sgh.de>
2016-05-17 11:33:20 +0200

    rework class FileOutManager

--- src/OpcUaHistory/HistoryAccessFile/FileOutManager.h
+++ src/OpcUaHistory/HistoryAccessFile/FileOutManager.h
@@ -8,9 +8,6 @@
 #ifndef __OpcUaHistory_FileOutManager_h__
 #define __OpcUaHistory_FileOutManager_h__
 
-#include "OpcUaStackCore/BuildInTypes/OpcUaDataValue.h"
-#include "OpcUaHistory/HistoryAccessFile/FileOut.h"
-
 using namespace OpcUaStackCore;
 
 namespace OpcUaHistory
@@ -21,36 +18,6 @@ namespace OpcUaHistory
 	  public:
 		FileOutManager(void);
 		~FileOutManager(void);
-
-		void basePath(const std::string& basePath);
-		std::string& basePath(void);
-		void maxNumberFilesPerDirectory(uint32_t maxNumberFilesPerDirectory);
-		uint32_t maxNumberFilesPerDirectory(void);
-		void maxFileSize(uint32_t);
-		uint32_t maxFileSize(void);
-		void maxOpenFiles(uint32_t maxOpenFiles);
-		uint32_t maxOpenFiles(void);
-
-		FileAccess::Result write(
-			const std::string& name,
-			OpcUaDataValue& dataValue
-		);
-
-	  private:
-		FileOut* createFileOutEntry(const std::string& name);
-		void removeFileOutEntry(FileOut* fileOut);
-		void updateFileOutEntry(FileOut* fileOut);
-
-		// configuration parameters
-		std::string basePath_;
-		uint32_t maxNumberFilesPerDirectory_;
-		uint32_t maxFileSize_;
-		uint32_t maxOpenFiles_;
-
-		// management parameter
-		FileOut::Map fileOutMap_;
-		DoublyLinkedList fileOutList_;
-
 	};
 
 }
