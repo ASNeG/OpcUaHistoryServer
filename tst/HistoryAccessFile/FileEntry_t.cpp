54d2fed81f7df713d7aa7db24955e238e875e80f
Kai Huebl <kai@huebl-sgh.de>
2016-09-13 08:28:12 +0200

    added environment to test file entry access via file

---
+++ tst/HistoryAccessFile/FileEntry_t.cpp
@@ -0,0 +1,14 @@
+#include "unittest.h"
+
+BOOST_AUTO_TEST_SUITE(FileEntry_)
+
+BOOST_AUTO_TEST_CASE(FileEntry_)
+{
+	std::cout << "FileEntry_t" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(FileEntry_xx)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
