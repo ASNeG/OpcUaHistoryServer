6fbcc5cef438733bb334d47ce8e92bbe05ea06dc
Kai Huebl <kai@huebl-sgh.de>
2016-09-29 08:54:10 +0200

    added new class ClientMonitoredItem

---
+++ src/OpcUaHistory/OpcUaClient/ClientMonitoredItem.h
@@ -0,0 +1,50 @@
+/*
+   Copyright 2016 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaHistory_ClientMonitoredItem_h__
+#define __OpcUaHistory_ClientMonitoredItem_h__
+
+#include <boost/shared_ptr.hpp>
+#include <map>
+#include <set>
+#include <vector>
+#include <stdint.h>
+
+
+namespace OpcUaHistory
+{
+
+	class ClientMonitoredItem
+	{
+	  public:
+		typedef boost::shared_ptr<ClientMonitoredItem> SPtr;
+		typedef std::map<std::string, ClientMonitoredItem::SPtr> Map;
+		typedef std::set<ClientMonitoredItem::SPtr> Set;
+		typedef std::vector<std::string> NamespaceUris;
+		typedef std::map<uint32_t, uint32_t> NamespaceMap;
+
+		typedef enum {
+			S_Error,
+		} State;
+
+		ClientMonitoredItem(void);
+		~ClientMonitoredItem(void);
+	};
+
+}
+
+#endif
