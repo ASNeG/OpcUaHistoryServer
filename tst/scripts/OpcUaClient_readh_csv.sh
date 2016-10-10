2f2508f227c56fc216b147e3c450f2bfb1e7e345
Kai Huebl <kai@huebl-sgh.de>
2016-10-10 14:11:58 +0200

    added localized text example

--- tst/scripts/OpcUaClient_readh_csv.sh
+++ tst/scripts/OpcUaClient_readh_csv.sh
@@ -6,3 +6,9 @@ OpcUaClient \
  -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
  -Command ReadH		-NodeId "ns=1;i=205" -StartTime "20001010T000000" -EndTime "23000101T000000" -TimestampsToReturn "Source" -MaxNumResultValuesPerNode 50 -MaxNumRequests 30 -csv "Value:Int16Array.csv"\
  -Command Disconnect 
+
+
+OpcUaClient \
+ -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
+ -Command ReadH		-NodeId "ns=1;i=227" -StartTime "20001010T000000" -EndTime "23000101T000000" -TimestampsToReturn "Source" -MaxNumResultValuesPerNode 50 -MaxNumRequests 30 -csv "Value:LocalizedTextArray.csv"\
+ -Command Disconnect 
