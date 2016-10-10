4a7ec07f1dbc42b9600f84a0c7d5bcc67938ba59
Kai Huebl <kai@huebl-sgh.de>
2016-10-10 13:08:16 +0200

    added test script to write historical read results into a csv file

---
+++ tst/scripts/OpcUaClient_readh_csv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+. ./TestConfig.sh
+
+OpcUaClient \
+ -Command Connect 	-Session "TestSession" -EndpointUrl ${EndpointUrl} \
+ -Command ReadH		-NodeId "ns=1;i=205" -StartTime "20001010T000000" -EndTime "23000101T000000" -TimestampsToReturn "Source" -MaxNumResultValuesPerNode 50 -MaxNumRequests 30 -csv "Value:Int16Array.csv"\
+ -Command Disconnect 
