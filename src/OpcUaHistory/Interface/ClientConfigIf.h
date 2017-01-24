3e1990ea8a2a2994e160592c2cde114998da5c89
Kai Huebl <kai@huebl-sgh.de>
2017-01-24 12:25:44 +0100

    added class ClientConfigIf

--- src/OpcUaHistory/Interface/ClientConfigIf.h
+++ src/OpcUaHistory/Interface/ClientConfigIf.h
@@ -15,17 +15,17 @@
    Autor: Kai Huebl (kai@huebl-sgh.de)
  */
 
-#ifndef __OpcUaHistory_ClientConfigIf_h__
-#define __OpcUaHistory_ClientConfigIf_h__
+#ifndef __OpcUaHistory_ServerConfigIf_h__
+#define __OpcUaHistory_ServerConfigIf_h__
 
 namespace OpcUaHistory
 {
 
-	class ClientConfigIf
+	class ServerConfigIf
 	{
 	  public:
-		ClientConfigIf(void) {}
-		virtual ~ClientConfigIf(void) {}
+		ServerConfigIf(void) {}
+		virtual ~ServerConfigIf(void) {}
 	};
 
 }
